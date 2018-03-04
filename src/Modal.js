3359d702c26a0e964cc21098242dec1a939d5082
fujisawakyle <fujisawakyle@gmail.com>
2018-03-03 20:00:34 -0800

    Update Modal.js
    add full screen modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,138 +1,284 @@
-import React,{ Component } from 'react';
-import ReactDOM from 'react-dom';
-import Assign from 'lodash.assign';
+'use strict';
 
-const prefix = require('react-prefixr');
+var _createClass = (function() {
+  function defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ('value' in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+  return function(Constructor, protoProps, staticProps) {
+    if (protoProps) defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) defineProperties(Constructor, staticProps);
+    return Constructor;
+  };
+})();
 
-const defaultStyles = {
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+exports.ModalManager = exports.Modal = undefined;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _lodash = require('lodash.assign');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError('Cannot call a class as a function');
+  }
+}
+
+function _possibleConstructorReturn(self, call) {
+  if (!self) {
+    throw new ReferenceError(
+      "this hasn't been initialised - super() hasn't been called"
+    );
+  }
+  return call && (typeof call === 'object' || typeof call === 'function')
+    ? call
+    : self;
+}
+
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== 'function' && superClass !== null) {
+    throw new TypeError(
+      'Super expression must either be null or a function, not ' +
+        typeof superClass
+    );
+  }
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      enumerable: false,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass)
+    Object.setPrototypeOf
+      ? Object.setPrototypeOf(subClass, superClass)
+      : (subClass.__proto__ = superClass);
+}
+
+var prefix = require('react-prefixr');
+
+var defaultStyles = {
   overlay: {
-    position        : 'fixed',
-    top             : 0,
-    left            : 0,
-    right           : 0,
-    bottom          : 0,
-    zIndex          : 99999999,
-    overflow        : 'hidden',
-    perspective     :  1300,
-    backgroundColor : 'rgba(0, 0, 0, 0.3)'
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 99999999,
+    overflow: 'hidden',
+    perspective: 1300,
+    backgroundColor: 'rgba(43, 43, 43, 0.3)'
   },
 
   content: {
-    position                : 'relative',
-    margin                  : '15% auto',
-    width                   : '60%',
-    border                  : '1px solid rgba(0, 0, 0, .2)',
-    background              : '#fff',
-    overflow                : 'auto',
-    borderRadius            : '4px',
-    outline                 : 'none',
-    boxShadow               : '0 5px 10px rgba(0, 0, 0, .3)',
+    position: 'relative',
+    margin: 'auto',
+    width: '100%',
+    height: '100%',
+    border: '1px solid rgba(0, 0, 0, .2)',
+    background: 'rgba(43, 43, 43, 1)',
+    color: '#fff',
+    overflow: 'auto',
+    borderRadius: '4px',
+    outline: 'none',
+    boxShadow: '0 5px 10px rgba(0, 0, 0, .3)'
   }
 };
 
-const defaultTransition = {
-   property : 'all',
-   duration : 300,
-   timingfunction : 'linear',
+var defaultTransition = {
+  property: 'all',
+  duration: 300,
+  timingfunction: 'linear'
 };
 
-const stopPropagation = (e) => e.stopPropagation();
+var stopPropagation = function stopPropagation(e) {
+  return e.stopPropagation();
+};
 
-let onClose;
+var onClose = void 0;
 
-export class Modal extends Component{
-   constructor(props){
-      super(props);
-      this.state = {
-         open : false
-      }
-   }
-   close(){
-       if(!this.props.onRequestClose || this.props.onRequestClose()){
+var Modal = (exports.Modal = (function(_Component) {
+  _inherits(Modal, _Component);
+
+  function Modal(props) {
+    _classCallCheck(this, Modal);
+
+    var _this = _possibleConstructorReturn(
+      this,
+      Object.getPrototypeOf(Modal).call(this, props)
+    );
+
+    _this.state = {
+      open: false
+    };
+    return _this;
+  }
+
+  _createClass(Modal, [
+    {
+      key: 'close',
+      value: function close() {
+        if (!this.props.onRequestClose || this.props.onRequestClose()) {
           ModalManager.close();
-       }
-   }
-   handleKeyDown(event){
-      if (event.keyCode == 27 /*esc*/) this.close();
-   }
-   componentDidMount(){
-      const transitionTimeMS = this.getTransitionDuration();
-      setTimeout(() => this.setState({open : true}),0);
-      onClose = (callback) => {
-         this.setState({open: false}, () => {
-           this.closeTimer = setTimeout(callback, transitionTimeMS);
-         });
-      };
-   }
-   componentWillUnmount(){
-      onClose = null;
-      clearTimeout(this.closeTimer);
-   }
-   getTransitionDuration(){
-     const { effect } = this.props;
-     if(!effect.transition){
-        return defaultTransition.duration;
-     }
-     return effect.transition.duration || defaultTransition.duration;
-   }
-   render(){
-      const {style,effect} = this.props;
-      const { open } = this.state;
-
-      let transition = effect.transition;
-      if(!transition){
-        transition = defaultTransition;
-      }else{
-        transition = Assign({},defaultTransition,transition);
+        }
       }
-      let transition_style = {
-        	'transition': transition.property+' '+(transition.duration / 1000) + 's'+' '+transition.timingfunction
-      };
+    },
+    {
+      key: 'handleKeyDown',
+      value: function handleKeyDown(event) {
+        if (event.keyCode == 27 /*esc*/) this.close();
+      }
+    },
+    {
+      key: 'componentDidMount',
+      value: function componentDidMount() {
+        var _this2 = this;
 
-      return (
-          <div
-            ref="overlay"
-            style={prefix(Assign({},defaultStyles.overlay,style ? (style.overlay ? style.overlay : {}) : {},{ transition: 'opacity '+(transition.duration / 1000) + 's'+' linear',opacity: open ? 1 : 0}))}
-            onClick={this.close.bind(this)}>
+        var transitionTimeMS = this.getTransitionDuration();
+        setTimeout(function() {
+          return _this2.setState({ open: true });
+        }, 0);
+        onClose = function onClose(callback) {
+          _this2.setState({ open: false }, function() {
+            _this2.closeTimer = setTimeout(callback, transitionTimeMS);
+          });
+        };
+      }
+    },
+    {
+      key: 'componentWillUnmount',
+      value: function componentWillUnmount() {
+        onClose = null;
+        clearTimeout(this.closeTimer);
+      }
+    },
+    {
+      key: 'getTransitionDuration',
+      value: function getTransitionDuration() {
+        var effect = this.props.effect;
 
-            <div
-              ref="content"
-              style={prefix(Assign({},defaultStyles.content,style ? (style.content ? style.content : {}) : {},transition_style,open ? effect.end : effect.begin))}
-              onClick={stopPropagation}
-              onKeyDown={this.handleKeyDown.bind(this)}>
-              {this.props.children}
-            </div>
-          </div>
-      );
-   }
-}
+        if (!effect.transition) {
+          return defaultTransition.duration;
+        }
+        return effect.transition.duration || defaultTransition.duration;
+      }
+    },
+    {
+      key: 'render',
+      value: function render() {
+        var _props = this.props;
+        var style = _props.style;
+        var effect = _props.effect;
+        var open = this.state.open;
+
+        var transition = effect.transition;
+        if (!transition) {
+          transition = defaultTransition;
+        } else {
+          transition = (0, _lodash2.default)({}, defaultTransition, transition);
+        }
+        var transition_style = {
+          transition:
+            transition.property +
+            ' ' +
+            transition.duration / 1000 +
+            's' +
+            ' ' +
+            transition.timingfunction
+        };
+
+        return _react2.default.createElement(
+          'div',
+          {
+            ref: 'overlay',
+            style: prefix(
+              (0, _lodash2.default)(
+                {},
+                defaultStyles.overlay,
+                style ? (style.overlay ? style.overlay : {}) : {},
+                {
+                  transition:
+                    'opacity ' + transition.duration / 1000 + 's' + ' linear',
+                  opacity: open ? 1 : 0
+                }
+              )
+            ),
+            onClick: this.close.bind(this)
+          },
+          _react2.default.createElement(
+            'div',
+            {
+              ref: 'content',
+              style: prefix(
+                (0, _lodash2.default)(
+                  {},
+                  defaultStyles.content,
+                  style ? (style.content ? style.content : {}) : {},
+                  transition_style,
+                  open ? effect.end : effect.begin
+                )
+              ),
+              onClick: stopPropagation,
+              onKeyDown: this.handleKeyDown.bind(this)
+            },
+            this.props.children
+          )
+        );
+      }
+    }
+  ]);
+
+  return Modal;
+})(_react.Component));
 
 var node;
 var modals = [];
 
-const renderModal = () => {
-   if(modals.length == 0)
-      return;
+var renderModal = function renderModal() {
+  if (modals.length == 0) return;
 
-   const component = modals.shift();
-   if(!node){
-      node = document.createElement('div');
-      document.body.appendChild(node);
-   }
-   ReactDOM.render(component,node);
-}
+  var component = modals.shift();
+  if (!node) {
+    node = document.createElement('div');
+    document.body.appendChild(node);
+  }
+  _reactDom2.default.render(component, node);
+};
 
-export const ModalManager = {
-    open(component){
-       modals.push(component);
-       if(modals.length == 1){ // render the modal only if there is no other showing modals
-          renderModal();
-       }
-    },
-    close(){
-       onClose && onClose(() => {
-         ReactDOM.unmountComponentAtNode(node);
-         renderModal();// render the other modals which are waiting.
-       });
+var ModalManager = (exports.ModalManager = {
+  open: function open(component) {
+    modals.push(component);
+    if (modals.length == 1) {
+      // render the modal only if there is no other showing modals
+      renderModal();
     }
-}
+  },
+  close: function close() {
+    onClose &&
+      onClose(function() {
+        _reactDom2.default.unmountComponentAtNode(node);
+        renderModal(); // render the other modals which are waiting.
+      });
+  }
+});
